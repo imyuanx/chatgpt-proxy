c16fcf8ff9c04b05a6559663a4f524aa6afec7d4
yuanx <yuanx.me@qq.com>
2023-03-06 20:59:37 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/proxy/:slug*",
+        destination: "https://api.openai.com/:slug*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
