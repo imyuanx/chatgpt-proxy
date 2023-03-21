04491bf128bf50dca75c6c58a647b454e79e3e3d
yuanx <yuanx.me@qq.com>
2023-03-21 16:32:24 +0800

    feat: Add copy button, update styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
+import ClipboardJS from "clipboard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   const [link, setLink] = useState("");
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     const { origin } = location;
     setLink(`${origin}/proxy`);
+    const clipboard = new ClipboardJS("#copy");
+    clipboard.on("success", notify);
+    return () => clipboard.destroy();
   }, []);
+
+  const notify = () => {
+    toast("🦄️ Copied !", { autoClose: 1600 });
+    setCopied(true);
+  };
 
   return (
     <>
@@ -20,13 +32,66 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <h1>ChatGPT Proxy</h1>
+          <div className={styles.titleBox}>
+            <h1 className={styles.title}>ChatGPT Proxy</h1>
+            <h1
+              className={styles.titleMask}
+              style={copied ? { height: 0 } : {}}
+            >
+              ChatGPT Proxy
+            </h1>
+          </div>
           <p>
-            Your proxy link:&nbsp;
+            Your proxy link:
             <code className={styles.code}>{link}</code>
+            <svg
+              id="copy"
+              className={styles.copyIcon}
+              data-clipboard-text={link}
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M9 18q-.825 0-1.412-.587Q7 16.825 7 16V4q0-.825.588-1.413Q8.175 2 9 2h9q.825 0 1.413.587Q20 3.175 20 4v12q0 .825-.587 1.413Q18.825 18 18 18Zm0-2h9V4H9v12Zm-4 6q-.825 0-1.413-.587Q3 20.825 3 20V7q0-.425.288-.713Q3.575 6 4 6t.713.287Q5 6.575 5 7v13h10q.425 0 .713.288q.287.287.287.712t-.287.712Q15.425 22 15 22ZM9 4v12V4Z"
+              />
+            </svg>
           </p>
         </div>
+        <div className={styles.faq}>
+          <h3>What's this?</h3>
+          <p>
+            <a
+              href="https://github.com/imyuanx/chatgpt-proxy#introduction"
+              target="_blank"
+            >
+              See more
+            </a>
+          </p>
+          <h3>How to use?</h3>
+          <p>
+            <a
+              href="https://github.com/imyuanx/chatgpt-proxy#how-to-use"
+              target="_blank"
+            >
+              See more
+            </a>
+          </p>
+        </div>
+        <footer className={styles.footer}>
+          {"yuanx@ "}
+          <a href="https://github.com/imyuanx" target="_blank">
+            GitHub
+          </a>
+          {" | "}
+          <a href="https://twitter.com/imyuanx" target="_blank">
+            Twitter
+          </a>
+        </footer>
       </main>
+      <ToastContainer />
     </>
   );
 }
